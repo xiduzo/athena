817cebf97bc7cc38705fff9450bd49a8b6fa32dc
sander boer <mail@sanderboer.nl>
2019-12-11 22:44:10 +0100

    removed some unused code

--- src/routes/agreements/style.ts
+++ src/routes/agreements/style.ts
@@ -1,7 +1,6 @@
 import { makeStyles } from '@material-ui/styles'
 
 import { Theme } from '@material-ui/core'
-import { red } from '@material-ui/core/colors'
 
 const drawerWidth = '20vw'
 export const useStyles = makeStyles((theme: Theme) => ({
@@ -14,19 +13,13 @@ export const useStyles = makeStyles((theme: Theme) => ({
     bottom: 0,
     right: drawerWidth,
     margin: theme.spacing(2),
+    zIndex: theme.zIndex.tooltip,
   },
-  main: {
+  root: {
     display: 'flex',
     padding: theme.spacing(2, 0),
   },
   toolbar: {
-    padding: theme.spacing(0, 1),
     ...theme.mixins.toolbar,
-  },
-  agreementTitle: {
-    fontSize: 14,
-  },
-  agreementAvatar: {
-    background: red[500],
   },
 }))
