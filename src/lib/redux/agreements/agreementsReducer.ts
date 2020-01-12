a2c5a8403dc404d6d8a762d07cbfc57c28d666af
sander boer <mail@sanderboer.nl>
2020-01-12 22:54:35 +0100

    redux working with agreement actions

--- src/lib/redux/agreements/agreementsReducer.ts
+++ src/lib/redux/agreements/agreementsReducer.ts
@@ -3,6 +3,9 @@ import { Status, IAction } from '../IRootReducer'
 
 export enum AgreementActions {
   setAgreements = 'setAgreements',
+  setAgreement = 'setAgreement',
+  addAgreement = 'addAgreement',
+  removeAgreement = 'removeAgreement',
 }
 
 export interface IAgreementsState {
@@ -24,6 +27,25 @@ export const agreementsReducer = (state: IAgreementsState = initial_state, actio
         ...state,
         status: Status.success,
         items: payload,
+      }
+    case AgreementActions.setAgreement:
+      return {
+        ...state,
+        items: state.items.map((item) => {
+          console.log(item)
+          if (item.id === payload.id) return payload as IAgreement
+          return item
+        }),
+      }
+    case AgreementActions.addAgreement:
+      return {
+        ...state,
+        items: [ ...state.items, payload as IAgreement ],
+      }
+    case AgreementActions.removeAgreement:
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== payload),
       }
     default:
       return state
