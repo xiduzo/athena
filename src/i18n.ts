2b33e694eeb9ec340f65517f52719a4cc1613bdd
sander boer <mail@sanderboer.nl>
2020-01-12 22:56:03 +0100

    format file

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,38 +3,32 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 import backend from 'i18next-xhr-backend'
 import { initReactI18next } from 'react-i18next'
 
-const locales = ['nl', 'en']
+const locales = [ 'nl', 'en' ]
 export const supportedLanguages = locales
-
-export const supportedLanguages = [ 'nl', 'en' ]
 
 const getLanguage = (): string => localStorage.getItem('i18nextLng') || 'en'
 
-i18n
-  .use(LanguageDetector)
-  .use(backend)
-  .use(initReactI18next)
-  .init({
-    backend: {
-      /* translation file path */
-      loadPath: '/assets/i18n/{{ns}}/{{lng}}.json',
-    },
-    cache: { enabled: true },
-    debug: true, // TODO: set false in production
-    defaultNS: 'translations',
-    fallbackLng: 'nl',
-    interpolation: {
-      escapeValue: false,
-      formatSeparator: ',',
-    },
-    keySeparator: false,
-    lng: getLanguage(), // choose any language from the list of translations
-    /* can have multiple namespace, in case you want to divide a huge
+i18n.use(LanguageDetector).use(backend).use(initReactI18next).init({
+  backend: {
+    /* translation file path */
+    loadPath: '/assets/i18n/{{ns}}/{{lng}}.json',
+  },
+  cache: { enabled: true },
+  debug: false, // TODO: set false in production
+  defaultNS: 'translations',
+  fallbackLng: 'nl',
+  interpolation: {
+    escapeValue: false,
+    formatSeparator: ',',
+  },
+  keySeparator: false,
+  lng: getLanguage(), // choose any language from the list of translations
+  /* can have multiple namespace, in case you want to divide a huge
        translation into smaller pieces and load them on demand */
-    ns: ['translations'],
-    react: {
-      wait: true,
-    },
-  })
+  ns: [ 'translations' ],
+  react: {
+    wait: true,
+  },
+})
 
 export default i18n
