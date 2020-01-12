e5fa95b2317fa548256600bcde20a2405958d39e
sander boer <mail@sanderboer.nl>
2020-01-12 22:55:43 +0100

    changed rootreducer and api endpoints

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const LDAP_URL = 'https://oege.ie.hva.nl/~vddxx/ldapper/index.php'
 export const BACKEND_URL = 'http://localhost:4000'
-export const TRIBE_ENDPOINT = 'world'
-export const SQUAD_ENDPOINT = 'guild'
+export const TRIBE_ENDPOINT = 'tribes'
+export const SQUAD_ENDPOINT = 'squads'
 export const ACCOUNT_ENDPOINT = 'user'
 export const AGREEMENTS_ENDPOINT = 'agreements'
 
--- src/lib/redux/IRootReducer.ts
+++ src/lib/redux/IRootReducer.ts
@@ -2,12 +2,14 @@ import { IUserState } from './user/IUserState'
 import { IGlobalState } from './global/IGlobalState'
 import { IAgreementsState } from './agreements/agreementsReducer'
 import { ITribesState } from './tribes/tribesReducer'
+import { ISquadsState } from './squads/squadsReducer'
 
 export interface IRootReducer {
   user: IUserState
   global: IGlobalState
   agreements: IAgreementsState
   tribes: ITribesState
+  squads: ISquadsState
 }
 
 export interface IAction {
