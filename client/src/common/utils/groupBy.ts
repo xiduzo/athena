03f2fb41a5f4ee0374e38cf0d4fa6c2ed9d5a1ea
sander boer <mail@sanderboer.nl>
2020-04-14 23:58:14 +0200

    added function type generic

--- client/src/common/utils/groupBy.ts
+++ client/src/common/utils/groupBy.ts
@@ -1,5 +1,7 @@
-export const groupBy = <T, K>(list: T[], keyGetter: (item: T) => K): Map<K, T[]> => {
-  const map = new Map()
+type KeyGetter<T, K> = (item: T) => K
+
+export const groupBy = <T, K>(list: T[], keyGetter: KeyGetter<T, K>): Map<K, T[]> => {
+  const map = new Map<K, T[]>()
 
   list.forEach((item) => {
     const key = keyGetter(item)
